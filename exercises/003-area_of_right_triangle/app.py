eb085ccf8c5b2054ead13d13c3c0040e05d0f192
natigalsan <natividadgalsan@gmail.com>
2022-05-18 11:11:04 +0000

    ejercicio 7

--- exercises/003-area_of_right_triangle/app.py
+++ exercises/003-area_of_right_triangle/app.py
@@ -1,10 +1,9 @@
 #Complete the function to return the area of the triangle.
-b = input("Size of B")
-h = input("Size of H")
+b = int(input("Size of B"))
+h = int(input("Size of H"))
 
 def area_of_triangle(arg1, arg2):
     #your code here, please remove the "None" 
-    return None
-
+    return arg1 * arg2 * 0.5
 # Testing your function
 print(area_of_triangle(b, h))
--- exercises/007-hours_and_minutes/app.py
+++ exercises/007-hours_and_minutes/app.py
@@ -1,7 +1,16 @@
 #Complete the function to calculate how many hours and minutes are passed since midnight.
 def hours_minutes(secs):
+
+  calculo_min = math.floor(secs / 60) 
+  calculo_horas = math.floor(calculo_min / 60)
+
+  return (calculo_horas, calculo_min)
+
   
-  return None
 
 #Invoke the funtion and pass any interger as its argument.
 print(hours_minutes(3900))
+
+
+#  1h = 60 min = 3600 sg
+#  1m = 60sg
